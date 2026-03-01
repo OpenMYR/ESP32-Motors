0102b915788f6ddc3a457daef25ef590cb137d21
Chris Adams <chris@openmyr.com>
2026-03-01 14:29:57 -0500

    unit test env tweaks

--- include/config/Config.h
+++ include/config/Config.h
@@ -1,7 +1,9 @@
 #ifndef MYR_CONFIG_H
 #define MYR_CONFIG_H
 
-#if __has_include("config/LocalConfig.h")
+#if defined(PIO_UNIT_TESTING)
+#include "config/DefaultConfig.h"
+#elif __has_include("config/LocalConfig.h")
 #include "config/LocalConfig.h"
 #else
 #include "config/DefaultConfig.h"
