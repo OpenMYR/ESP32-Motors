d8e3bd9b937a4344842f9f4666e2be37d33990f8
Chris Adams <chris@openmyr.com>
2026-03-07 16:43:01 -0500

    Stepper Control UI fix for out of order opcode

--- data/web_step/StepperMotor.js
+++ data/web_step/StepperMotor.js
@@ -25,7 +25,7 @@ class StepperMotor {
 	}
 
 	_mousePressed(event) {
-		StepperMotor.setMicroStepping(true);
+		this.queueMicroStepping(true);
 		this._mousedown = true; 
 		this._mouseMoved(event);
 	}
@@ -51,9 +51,21 @@ class StepperMotor {
 	}
 
 	_touchStart(event) {
-		StepperMotor.setMicroStepping(true);
+		this.queueMicroStepping(true);
 		this._touchdown = true;
 		this._touchMoved(event);
+	}
+
+	queueMicroStepping(flag) {
+		this.virtualMotor.queueCommand({
+			code : "U",
+			data : [
+				this.motorArgs.motorid,
+				0,
+				0,
+				flag ? 1 : 0,
+			]
+		});
 	}
 
 	_touchMoved(event) {
@@ -171,21 +183,4 @@ class StepperMotor {
 		ctx.restore();
 	}
 
-	static setMicroStepping(flag) {
-	let out = {
-		commands : [{
-			code : "U",
-			data : [
-				motor1.motorid,
-				0,
-				0,
-				flag ? 1 : 0,
-			]
-		}]
-	};
-	let httpRequest = new XMLHttpRequest();
-	httpRequest.open("POST", "/", true);
-	httpRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	httpRequest.send(JSON.stringify(out));
-}
 }
--- data/web_step/VirtualMotor.js
+++ data/web_step/VirtualMotor.js
@@ -4,6 +4,7 @@ class VirtualMotor {
 		this.transmitInterval = args.transmitInterval;
 		this.motorPositions = args.motorPositions.slice();
 		this.oldMotorPositions = args.motorPositions.slice();
+		this.pendingCommands = [];
 		this.changed = false;
 		this.send();
 		this.launchLoop();
@@ -23,6 +24,12 @@ class VirtualMotor {
 		this.tryUpdate();
 	}
 
+	queueCommand(command) {
+		this.pendingCommands.push(command);
+		this.changed = true;
+		this.tryUpdate();
+	}
+
 	tryUpdate() {
 		if (this.changed) {
 			if (Date.now() >= this.lastTransmition + this.transmitInterval) {
@@ -33,7 +40,8 @@ class VirtualMotor {
 	}
 
 	send() {
-		let commands = [];
+		let commands = this.pendingCommands.slice();
+		this.pendingCommands = [];
 		for (let i = 0; i < this.motorPositions.length; i++) {
 			if(this.motorPositions[i] != this.oldMotorPositions[i]) {
 				commands.push({
